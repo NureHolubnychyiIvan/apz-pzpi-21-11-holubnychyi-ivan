3f07d252aa97de27d26af176fb5d9bb8c1db1b76
NureHolubnychyiIvan <147333529+NureHolubnychyiIvan@users.noreply.github.com>
2024-06-28 01:02:32 +0300

    Delete src directory

--- src/main/java/com/system/fridges/models/transferObjects/authenticationObjects/RefreshTokenRequest.java
+++
@@ -1,9 +0,0 @@
-package com.system.fridges.models.transferObjects.authenticationObjects;
-
-import lombok.Data;
-
-@Data
-public class RefreshTokenRequest {
-
-    private String token;
-}
--- src/main/java/com/system/fridges/repositories/ModelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.system.fridges.repositories;
-
-import com.system.fridges.models.entities.Model;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModelRepository extends JpaRepository<Model, Integer> {
-}
